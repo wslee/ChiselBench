0fafd60424cea4a74ffdc41e4bd66a3328c0b8ec
Woosuk Lee <bear07291@gmail.com>
2019-03-11 11:42:52 +0900

    initial commit (new benchmarks)

---
+++ benchmark/wc-8.1/test.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+export BENCHMARK_NAME=wc-8.1 # TODO
+export BENCHMARK_DIR=$CHISEL_BENCHMARK_HOME/benchmark/$BENCHMARK_NAME
+export SRC=$BENCHMARK_DIR/$BENCHMARK_NAME.c
+export ORIGIN_BIN=$BENCHMARK_DIR/$BENCHMARK_NAME.origin
+export REDUCED_BIN=$BENCHMARK_DIR/$BENCHMARK_NAME.reduced
+export TIMEOUT=1 # TODO: properly modify this
+export LOG=$BENCHMARK_DIR/log.txt
+
+source $CHISEL_BENCHMARK_HOME/benchmark/test-base.sh
+
+function clean() {
+  # TODO
+  return 0
+}
+
+function desired() {
+  # TODO
+  return 0
+}
+
+function undesired() {
+  # TODO
+  return 0
+}
+
+function desired_disaster() {
+  # TODO
+  return 0
+}
+
+main
