5fde7288321bad02bb6cc87bc7c15b8a566ceb99
Woosuk Lee <bear07291@gmail.com>
2019-04-10 12:33:42 +0900

    afl cmd

--- benchmark/target.mk
+++ benchmark/target.mk
@@ -9,6 +9,15 @@ reduce: lib
 	chisel ./$(ORACLE) $(SRC)
 	cp $(SRC).chisel.c $(SRC).reduced.c
 
+$(BIN).reduced.afl.cmd: $(SRC).reduced.c.afl.cmd.c
+	afl-$(CC) -w -o $@ $(SRC).reduced.c.afl.cmd.c $(LFLAGS)
+
+afl-cmd-reduced: $(BIN).reduced.afl.cmd
+	rm -rf afl_cmd_output_reduced
+	mkdir afl_cmd_output_reduced
+	export AFL_SKIP_CPUFREQ=1
+	unset ASAN_OPTIONS; timeout 259200 afl-fuzz -i afl_input -o afl_cmd_output_reduced ./$(BIN).reduced.afl.cmd
+
 clean:
 	rm -f $(NAME).origin $(NAME).reduced $(NAME).c
 
